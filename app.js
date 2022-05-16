36a5d025c8cb022675c019a01abc1e1fc8921ed3
Bnyro <yannick.fehler@web.de>
2022-05-16 18:34:57 +0200

    Cleanup

--- app.js
+++ app.js
@@ -33,7 +33,7 @@
 const themeToggleBtn = document.getElementById('toggleTheme');
 
 themeToggleBtn.addEventListener('click', () => {
-    document.querySelector('html').classList.toggle('light');
+  document.querySelector('html').classList.toggle('light');
 });
 
 /* ScrollToTOp Button */
@@ -41,22 +41,22 @@ themeToggleBtn.addEventListener('click', () => {
 const topBtn = document.getElementById('topBtn');
 
 window.onscroll = function() {
-    scrollFunction()
+  scrollFunction()
 };
 scrollFunction();
 
 function scrollFunction() {
-    scrollPos = window.scrollY;
-    if (scrollPos > 20) {
-        topBtn.style.display = "block";
-    } else {
-        topBtn.style.display = "none";
-    }
+  scrollPos = window.scrollY;
+  if (scrollPos > 20) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
 }
 
 topBtn.addEventListener('click', () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
 
 /* Navbar Toggle */
@@ -66,74 +66,74 @@ const navlist = document.getElementsByClassName('nav-list')[0];
 const navlinks = document.querySelectorAll('.nav-list li');
 
 toggleButton.addEventListener('click', () => {
-    switchToggle();
+  switchToggle();
 });
 
 navlinks.forEach(navlink => {
-    navlink.addEventListener('click', () => {
-        switchToggle();
-    });
+  navlink.addEventListener('click', () => {
+    switchToggle();
+  });
 });
 
 function switchToggle() {
-    navlist.classList.toggle('active');
-    toggleButton.innerHTML = toggleButton.innerHTML === '×' ? '≡' : '×';
+  navlist.classList.toggle('active');
+  toggleButton.innerHTML = toggleButton.innerHTML === '×' ? '≡' : '×';
 }
 
 /* Copy to Clipboard */
 
 const donationBtns = document.querySelectorAll('#faq p')
 donationBtns.forEach(donationBtn => {
-    donationBtn.addEventListener('click', () => {
-        var text = donationBtn.querySelector('span').innerHTML
-        copyToClipboard(text);
-    });
+  donationBtn.addEventListener('click', () => {
+    var text = donationBtn.querySelector('span').innerHTML
+    copyToClipboard(text);
+  });
 });
 
 function copyToClipboard(text) {
-    Clipboard.copy(text);
-    alert("Copied: " + text);
+  Clipboard.copy(text);
+  alert("Copied: " + text);
 }
 
 window.Clipboard = (function(window, document, navigator) {
-    var textArea, copy;
+  var textArea, copy;
 
-    function isOS() {
-        return navigator.userAgent.match(/ipad|iphone/i);
-    }
+  function isOS() {
+    return navigator.userAgent.match(/ipad|iphone/i);
+  }
 
-    function createTextArea(text) {
-        textArea = document.createElement('textArea');
-        textArea.value = text;
-        document.body.appendChild(textArea);
-    }
+  function createTextArea(text) {
+    textArea = document.createElement('textArea');
+    textArea.value = text;
+    document.body.appendChild(textArea);
+  }
 
-    function selectText() {
-        var range, selection;
-        if (isOS()) {
-            range = document.createRange();
-            range.selectNodeContents(textArea);
-            selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
-            textArea.setSelectionRange(0, 999999);
-        } else {
-            textArea.select();
-        }
+  function selectText() {
+    var range, selection;
+    if (isOS()) {
+      range = document.createRange();
+      range.selectNodeContents(textArea);
+      selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      textArea.setSelectionRange(0, 999999);
+    } else {
+      textArea.select();
     }
+  }
 
-    function copyToClipboard() {
-        document.execCommand('copy');
-        document.body.removeChild(textArea);
-    }
-    copy = function(text) {
-        createTextArea(text);
-        selectText();
-        copyToClipboard();
-    };
-    return {
-        copy: copy
-    };
+  function copyToClipboard() {
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+  }
+  copy = function(text) {
+    createTextArea(text);
+    selectText();
+    copyToClipboard();
+  };
+  return {
+    copy: copy
+  };
 })(window, document, navigator);
 
 /* Text Change */
@@ -144,47 +144,43 @@ const changeText = document.getElementById('changeText');
 changeString();
 
 function changeString() {
-    changeStrings.forEach(function(string, index) {
-        setTimeout(() => {
-            changeText.innerHTML = string;
-        }, index * 2000);
-    });
-    setTimeout(changeString, 10000)
+  changeStrings.forEach(function(string, index) {
+    setTimeout(() => {
+      changeText.innerHTML = string;
+    }, index * 2000);
+  });
+  setTimeout(changeString, 10000)
 }
 
-// ScrollFade 0.1
+// Scroll Fade
 
-var fadeElements = document.getElementsByClassName('scrollFade');
+var fadeElements = document.querySelectorAll('.scrollFade');
 
 function scrollFade() {
-	var viewportBottom = window.scrollY + window.innerHeight;
+  fadeElements.forEach(element => {
+    var rect = element.getBoundingClientRect();
+    var elementFourth = rect.height / 4;
+    var fadeInPoint = window.innerHeight - elementFourth;
+    var fadeOutPoint = -(rect.height / 2);
 
-	for (var index = 0; index < fadeElements.length; index++) {
-		var element = fadeElements[index];
-		var rect = element.getBoundingClientRect();
+    if (rect.top <= fadeInPoint) {
+      element.classList.add('scrollFade--visible');
+      element.classList.add('scrollFade--animate');
+      element.classList.remove('scrollFade--hidden');
+    } else {
+      element.classList.remove('scrollFade--visible');
+      element.classList.add('scrollFade--hidden');
+    }
 
-		var elementFourth = rect.height/4;
-		var fadeInPoint = window.innerHeight - elementFourth;
-		var fadeOutPoint = -(rect.height/2);
-
-		if (rect.top <= fadeInPoint) {
-			element.classList.add('scrollFade--visible');
-			element.classList.add('scrollFade--animate');
-			element.classList.remove('scrollFade--hidden');
-		} else {
-			element.classList.remove('scrollFade--visible');
-			element.classList.add('scrollFade--hidden');
-		}
-
-		if (rect.top <= fadeOutPoint) {
-			element.classList.remove('scrollFade--visible');
-			element.classList.add('scrollFade--hidden');
-		}
-	}
+    if (rect.top <= fadeOutPoint) {
+      element.classList.remove('scrollFade--visible');
+      element.classList.add('scrollFade--hidden');
+    }
+  });
 }
 
 document.addEventListener('scroll', scrollFade);
 window.addEventListener('resize', scrollFade);
 document.addEventListener('DOMContentLoaded', function() {
-    scrollFade();
+  scrollFade();
 });
