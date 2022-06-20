7a91e697dc556556ccdace6a3feef449cad1e10f
Bnyro <yannick.fehler@web.de>
2022-06-20 21:03:06 +0200

    faq redesign + footer

---
+++ app.js
@@ -0,0 +1,16 @@
+/* faq: close other details when one is opened */
+
+// Fetch all the details element.
+const details = document.querySelectorAll("details");
+
+// Add the onclick listeners.
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener("click", () => {
+    // Close all the details that are not targetDetail.
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
